28c4946c2f62930ef974aa6d369bcb259da8cc53
Vergel James Viray <vergeljamesviray@gmail.com>
2025-01-24 11:49:07 +0800

    feat: generated and imported initial spring boot application

---
+++ sample-spring-boot-project/src/test/java/com/example/sample_spring_boot_project/SampleSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sample_spring_boot_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
